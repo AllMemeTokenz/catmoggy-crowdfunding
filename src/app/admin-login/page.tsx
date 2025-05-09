a76667ef587300e82b22a9a2ad66f3a375d71f45
RafifMMR <rafifmuhammad2003@gmail.com>
2025-05-09 20:05:10 +0700

    add login page

---
+++ src/app/admin-login/page.tsx
@@ -0,0 +1,5 @@
+import LoginCard from "@/components/layout/login-card";
+
+export default function LoginPage() {
+  return <LoginCard />;
+}
--- src/app/dashboardzzz/layout.tsx
+++ src/app/dashboardzzz/layout.tsx
@@ -58,7 +58,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
         </nav>
         <div className="absolute bottom-0 w-full p-4 border-t border-gray-200">
           <Link
-            href="/logout"
+            href="/admin-login"
             className="flex items-center gap-3 rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100"
           >
             <LogOut className="h-5 w-5 text-gray-500" />
